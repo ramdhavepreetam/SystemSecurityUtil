f9feb8ec9bd9aeebbe69379e948e2bd7522f84ff
Preetam <ramdhavepreetam@gmail.com>
2016-11-07 09:37:16 -0800

    New utility Class added

--- SystemSecurityUtil/ProcessUtility.Designer.cs
+++ SystemSecurityUtil/ProcessUtility.Designer.cs
@@ -28,25 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.cmdVarifysys = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // cmdVarifysys
             // 
-            this.button1.Location = new System.Drawing.Point(125, 69);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(166, 73);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.cmdVarifysys.Location = new System.Drawing.Point(62, 78);
+            this.cmdVarifysys.Name = "cmdVarifysys";
+            this.cmdVarifysys.Size = new System.Drawing.Size(166, 73);
+            this.cmdVarifysys.TabIndex = 0;
+            this.cmdVarifysys.Text = "button1";
+            this.cmdVarifysys.UseVisualStyleBackColor = true;
+            this.cmdVarifysys.Click += new System.EventHandler(this.button1_Click);
             // 
             // ProcessUtility
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(578, 303);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(625, 329);
+            this.Controls.Add(this.cmdVarifysys);
             this.Name = "ProcessUtility";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -55,7 +55,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button cmdVarifysys;
     }
 }
 
