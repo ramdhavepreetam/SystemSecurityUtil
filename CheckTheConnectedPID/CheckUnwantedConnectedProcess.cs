75b0c59ba6ce07541529ad41f27c74358370917b
A-Preetam.Ramdhave <A-Preetam.Ramdhave@ITDRENLPP15179>
2016-11-07 09:18:59 -0800

    Add project files.

---
+++ CheckTheConnectedPID/CheckUnwantedConnectedProcess.cs
@@ -0,0 +1,49 @@
+﻿///-----------------------------------------------------------------
+///   Namespace:      <CheckTheConnectedPID>
+///   Class:          <CheckUnwantedConnectedProcess>
+///   Description:    <This Class is used to extract the Current Connected process>
+///   Author:         <Preetam Ramdhave>                    Date: <2-OCT-2016>
+///   Notes:          <This class will give me all the procoess via netstat and get the 
+///                    this class will refine the data and give us the proper data.>
+///   Revision History:
+///   Name:           Date:        Description:
+///----
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckTheConnectedPID
+{
+    public class CheckUnwantedConnectedProcess
+    {
+public        string GetCurrectProcessData()
+        {
+            var proc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "netstat.exe",
+                    Arguments = " -ano",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }   
+            };
+            proc.Start();
+            string line = string.Empty ;
+            while (!proc.StandardOutput.EndOfStream)
+            {
+                  line += proc.StandardOutput.ReadLine();
+                // do something with line
+            }
+          
+            proc.Dispose();
+
+            return line;
+        }
+    }
+}
---
+++ SystemSecurityUtil/ProcessUtility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CheckTheConnectedPID;
+namespace CheckSystemSecurity
+{
+    public partial class ProcessUtility : Form
+    {
+        public ProcessUtility()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CheckUnwantedConnectedProcess objproce = new CheckUnwantedConnectedProcess();
+            MessageBox.Show(objproce.GetCurrectProcessData());
+        }
+    }
+}
